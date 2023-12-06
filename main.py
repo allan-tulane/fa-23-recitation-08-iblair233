66dc9f38ee35a3e8c1b14c09b861653731386208
iblair233 <126489949+iblair233@users.noreply.github.com>
2023-12-05 20:07:28 -0600

    done?

--- main.py
+++ main.py
@@ -13,10 +13,27 @@ def shortest_shortest_path(graph, source):
       (shortest path weight, shortest path number of edges). See test case for example.
     """
     ### TODO
-    pass
+    frontier = []
+    heappush(frontier, (0, 0, source))
+    visited = dict()
     
-
+    def helper(visited, frontier):
+        if len(frontier) == 0:
+            return visited
+        else:
+            # adjacent one closest to heap
+            distance, edges, node = heappop(frontier)
+            if node in visited:
+                return helper(visited, frontier)
+            else:
+                visited[node] = (distance, edges)
+                for neighbor, weight in graph[node]:
+                    heappush(frontier, (distance+weight, edges+1, neighbor))
+                return helper(visited,frontier)
     
+    return helper(visited,frontier)
+    
+  
     
 def bfs_path(graph, source):
     """
@@ -25,7 +42,25 @@ def bfs_path(graph, source):
       that vertex in the shortest path tree.
     """
     ###TODO
-    pass
+    parents = dict() 
+    frontier = deque()
+    frontier.append(source)
+    visited = set()
+    def bfs_serial_helper(visited, frontier, parents):
+        if len(frontier) == 0:
+            return parents
+        else:
+            node = frontier.popleft()
+            visited.add(node)
+            for n in graph[node]:
+              if n not in visited and n not in frontier:
+                parents[n] = node 
+                frontier.append(n)
+            return bfs_serial_helper(visited, frontier, parents)
+            
+    return bfs_serial_helper(visited, frontier, parents)
+
+
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -44,5 +79,7 @@ def get_path(parents, destination):
       (excluding the destination node itself). See test_get_path for an example.
     """
     ###TODO
-    pass
-
+    if destination in parents:
+        return get_path(parents, parents[destination]) + parents[destination]
+    else:
+        return ''
